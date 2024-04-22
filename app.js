8074545ce904da8ded362dc3c4fda6a2a9c735f7
MuammadAnusAhmed <MUHAMMADANUSAHMEDON@gmail.com>
2024-04-22 15:46:15 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+var names = ["muhammad", "anus", "ahmed"];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+let names :string[] = ["muhammad" , "anus" , "ahmed"];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
